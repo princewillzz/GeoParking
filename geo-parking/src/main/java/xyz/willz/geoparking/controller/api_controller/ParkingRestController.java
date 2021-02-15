73834efa17e1a7eece6bad738e10c529432825e7
princewillzz <princewillz2013@gmail.com>
2021-02-16 02:05:55 +0530

    testing from ubuntu

--- geo-parking/src/main/java/xyz/willz/geoparking/controller/api_controller/ParkingRestController.java
+++ geo-parking/src/main/java/xyz/willz/geoparking/controller/api_controller/ParkingRestController.java
@@ -3,7 +3,6 @@ package xyz.willz.geoparking.controller.api_controller;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.apache.catalina.connector.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import xyz.willz.geoparking.dto.ParkingDTO;
