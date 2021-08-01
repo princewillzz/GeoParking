d176a62a9758406c141772be5c3a7566b03e4cf6
princewillzz <princewillz2013@gmail.com>
2021-08-01 15:24:11 +0530

    changing the app service

--- frontend/geoparking/src/api/axios-config.js
+++ frontend/geoparking/src/api/axios-config.js
@@ -4,8 +4,8 @@ import { ENV, ENVIRONMENTS } from "../config";
 let baseURL;
 
 if (ENV === ENVIRONMENTS.PRODUCTION) {
-	// baseURL = "https://geoparking-gateway.herokuapp.com";
-	baseURL = "https://parking-gateway-server.herokuapp.com";
+	baseURL = "https://geoparking-gateway.herokuapp.com";
+	// baseURL = "https://parking-gateway-server.herokuapp.com";
 } else if (ENV === ENVIRONMENTS.DEVELOPMENT) {
 	baseURL = "http://localhost:8080";
 }
