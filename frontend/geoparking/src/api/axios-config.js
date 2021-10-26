6ea450243ab6b5246748a85c5f6367637cc506a8
Harsh Tiwari <princewillz2013@gmail.com>
2021-10-26 22:40:53 +0530

    axios config update to dynamically change gateway

--- frontend/geoparking/src/api/axios-config.js
+++ frontend/geoparking/src/api/axios-config.js
@@ -4,7 +4,11 @@ import { ENV, ENVIRONMENTS } from "../config";
 let baseURL;
 
 if (ENV === ENVIRONMENTS.PRODUCTION) {
-	baseURL = "https://geoparking-gateway.herokuapp.com";
+	if(new Date().getDate() < 25)
+		baseURL = "https://geoparking-gateway.herokuapp.com";
+	else 
+		baseURL = "https://parking-gateway-server.herokuapp.com";
+// 	baseURL = "https://geoparking-gateway.herokuapp.com";
 	// baseURL = "https://parking-gateway-server.herokuapp.com";
 } else if (ENV === ENVIRONMENTS.DEVELOPMENT) {
 	baseURL = "http://localhost:8080";
