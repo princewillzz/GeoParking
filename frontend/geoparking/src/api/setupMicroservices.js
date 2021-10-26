1add2614928bc1f6af22464fa50c86489f6cf6e7
Harsh Tiwari <princewillz2013@gmail.com>
2021-10-26 22:41:56 +0530

    update wake microservices endpoints

--- frontend/geoparking/src/api/setupMicroservices.js
+++ frontend/geoparking/src/api/setupMicroservices.js
@@ -18,8 +18,15 @@ export const wakeUpApiGateway = async () => {
 
 // waking parknig service
 export const wakeUpParkingService = async () => {
+
+	let uri = "https://geoparking-parking.herokuapp.com/internal/awake";
+
+	if(new Date().getDate() >= 25) {
+		uri = "https://parking-parking-service.herokuapp.com/internal/awake"
+	}
+
 	return axios
-		.get("https://geoparking-parking.herokuapp.com/internal/awake")
+		.get(uri)
 		.then(() => true)
 		.catch(() => {
 			throw new Error("Sleeping slothly");
@@ -28,8 +35,14 @@ export const wakeUpParkingService = async () => {
 
 // waking profile service
 export const wakeUpProfileService = async () => {
+	let uri = "https://geoparking-profile.herokuapp.com/internal/awake";
+
+	if(new Date().getDate() >= 25) {
+		uri = "https://parking-profile-service.herokuapp.com/internal/awake"
+	}
+
 	return axios
-		.get("https://geoparking-profile.herokuapp.com/internal/awake")
+		.get(uri)
 		.then(() => true)
 		.catch(() => {
 			throw new Error("Sleeping slothly");
@@ -38,8 +51,14 @@ export const wakeUpProfileService = async () => {
 
 // waking booking service
 export const wakeUpBookingService = async () => {
+	let uri = "https://geoparking-booking.herokuapp.com/internal/awake";
+
+	if(new Date().getDate() >= 25) {
+		uri = "https://parking-booking-service.herokuapp.com/internal/awake"
+	}
+
 	return axios
-		.get("https://geoparking-booking.herokuapp.com/internal/awake")
+		.get(uri)
 		.then(() => true)
 		.catch(() => {
 			throw new Error("Sleeping slothly");
